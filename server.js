1aa3e3e126c613b5dea9213b6ed63cca8c21f95a
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2022-06-29 15:14:43 -0400

    some broken things fixed. Movie info now comes in just need to fix rendering of weather and movie data

--- server.js
+++ server.js
@@ -4,64 +4,45 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+// const movies = require('./data/movies.json');  //don't have this file yet
 
-const movies = require('./data/movies.json');
 // const { response } = require('express');
 // const { request } = require('http');
 
 const app = express();
 app.use(cors());
-
 const PORT = process.env.PORT || 3002;
-
 app.get('/', (request, response) => { // the slash denotes http://localhost:3000/  <-- see that last slash? that's the one!
-
-  response.send('Happiness is a full tank of gas nowhere to be and all day to get there!');
+  response.send('Proof of Life: Happiness is a full tank of gas nowhere to be and all day to get there!');
 });
 
-// Create an API endpoint of `/ movies` that processes a `GET`` request that contains`lat`, `lon` and `searchQuery` information.
-app.get('/movies', async (request, response) => {
+
+app.get('/weather', async (request, response) => {
   let lat = request.query.lat;
   let lon = request.query.lon;
-  console.log('proof of life that the code is getting this far!');
-  // let searchQuery = request.query.searchQuery;
-
-  // for lab 8 we'll no longer be using this to find the searched for city;  Use the .find() method to discover which city the `lat`, `lon` and `searchQuery` information belong to.
-
-  // const city = movies.find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase());
-
-  // this find method searches over the movies data and sends back any city therein whose name is exactly equal to what was searched for as a lower case string
-
+  let searchQuery = request.query.searchQuery;
+  // const city = weather.find(cityObj.city_name.toLowerCase() === searchQuery.toLowerCase()); // lab 7 only
   try {
-    //Using each ata point from the static data of the city that the user searched, create an array of 'Forecast' objects, one for each day.
-    const url = `https://api.moviesbit.io/v2.0/forecast/daily?key=${process.env.movies_API_KEY}&days=3&lat=${lat}&long=${lon}`;
-
-    console.log('URL: ', url);
-    const moviesData = await axios.get(url);
-    console.log(moviesData.data);
-
-    const moviesArray = moviesData.data.data.map(day => new Forecast(day));
-
-    // Send the full array back to the client who requestd data from the `/movies` endpoint.
-    response.status(200).send(moviesArray); //.send sends whatever is in parens back up to the client in this case it is sending the resulting mapped moviesArray back
-
+    const weatherArray = city.data.map(day => new Forecast(day));
+    response.status(200).send(weatherArray);
+    // const url = `https://api.moviesbit.io/v2.0/forecast/daily?key=${process.env.MOVIE_API_KEY}&days=3&lat=${lat}&long=${lon}`;
+    // console.log('URL: ', url);
+    // const moviesData = await axios.get(url);
+    // console.log(moviesData.data);
   } catch (error) {
-    // If the user did not search for one of the three cities that we have information about (Seattle, Paris, or Amman), return an error.
     console.error(error);
-    response.status(500).send('city not found'); //code 500 indicates a local server error
+    response.status(500).send('city not found');
   }
 });
 
-// class for `Forecast`, that has properties of `date` and `description`
-class Forecast {
+class Forecast {   // class for `Forecast`, that has properties of `date` and `description`
   constructor(day) {
     this.day = day.valid_date;
     this.description = day.movies.description;
   }
 }
 
-// Adding movie endpoint
-app.get('/movies', async (request, response) => {
+app.get('/movies', async (request, response) => {  // Adding movie endpoint
   let searchQuery = request.query.searchQuery;
   try {
     const url = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.MOVIE_API_KEY}&query=$[searchQuery}&language-en-US&page=1`;
@@ -88,15 +69,6 @@ class Movies {
   }
 }
 
-// we COULD put the get on line 31 inside of this class as a method but it is not required (it was done that way in class 7 demo)
+app.use('*', (request, response) => response.status(404).send('that endpoint does not exist'));  // Wildcard error (from any API call) handling
 
-// Remember, both classes and constructor functions both make objects, this function is equivalent to the class above
-// function Forecast(day) {
-//   this.day = day.valid_date;
-//   this.description = day.movies.description;
-
-// Create a function to handle errors from any API call. Wildcard handling.
-app.use('*', (request, response) => response.status(404).send('that endpoint does not exist'));
-
-// tells the express app which port to listen on
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`)); // tells the express which port to listen on
